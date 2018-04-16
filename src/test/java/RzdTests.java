006cb908132fdcbdc0039613d9ff09f78f9a47e4
Lyuba <vlladanna@yandex.ru>
2018-04-17 01:30:59 +0300

    Reading from config file was added

--- src/test/java/RzdTests.java
+++ src/test/java/RzdTests.java
@@ -2,19 +2,59 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.openqa.selenium.support.PageFactory;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
 
 public class RzdTests {
     private WebDriver driver;
 
+    private Properties getProperties() {
+        File file = new File("src/dataFile.properties");
+        FileInputStream fileInput = null;
+
+        try {
+            fileInput = new FileInputStream(file);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        Properties prop = new Properties();
+
+        try {
+            prop.load(fileInput);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return prop;
+    }
+
     @Before
     public void setUp() {
-        String driverPath = "C:\\geckodriver.exe";
-        String driverName = "webdriver.gecko.driver";
-        String url = "http://pass.rzd.ru/";
+        Properties prop = getProperties();
+
+        String driverPath = prop.getProperty("driverPath");
+        String driverName = prop.getProperty("driverName");
+        String url = prop.getProperty("url");
+
         System.setProperty(driverName, driverPath);
-        driver = new FirefoxDriver();
+
+        if (driverName.equals("webdriver.gecko.driver")) {
+            driver = new FirefoxDriver();
+        } else if (driverName.equals("webdriver.chrome.driver")) {
+            driver = new ChromeDriver();
+        } else if (driverName.equals("webdriver.ie.driver")) {
+            driver = new InternetExplorerDriver();
+        } else {
+            throw new IllegalArgumentException("Browser " + driverName + " not found!");
+        }
         driver.get(url);
     }
 
